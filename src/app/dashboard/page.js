26cef55e4d79d52da3f3ebf705a9c092f5d92e89
RahulJaincorpseed <rahul.jain@corpseed.com>
2024-02-06 17:34:52 +0530

    add next app

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export default function DashBoard() {
+  return (
+    <>
+      <Link href="/home">GO to Home</Link>
+      <p>DashBoard</p>
+    </>
+  )
+}
---
+++ src/app/home/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Home() {
+    return (
+        <>
+           <Link href="/dashboard">GO to Dashboard</Link>
+        <p>Home</p>
+        </>
+    )
+}
