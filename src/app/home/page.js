6f3dbfb2af124b9ffaf51286f673d269c896a045
RahulJaincorpseed <rahul.jain@corpseed.com>
2024-02-08 16:47:02 +0530

    add framer motion

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,10 +1,22 @@
-import Link from "next/link";
+"use client"
+import Link from "next/link"
+import { motion } from "framer-motion"
 
 export default function Home() {
-    return (
-        <>
-           <Link href="/dashboard">GO to Dashboard</Link>
-        <p>Home</p>
-        </>
-    )
+  return (
+    <div className="main-box">
+      <Link href="/dashboard">GO to Dashboard</Link>
+      <p>Home page</p>
+      <button>click me</button>
+      <motion.div
+        animate={{
+          x: 200,
+          y: 100,
+          scale: 1.2,
+        }}
+      >
+        <div className="my-box"></div>
+      </motion.div>
+    </div>
+  )
 }
